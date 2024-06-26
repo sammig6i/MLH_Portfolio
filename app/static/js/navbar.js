0fd0c3f505ebba1de1132bb7b2e24cd90f1efdc6
SavviSam <swghazzawi@gmail.com>
2024-06-26 16:11:08 -0600

    Exprience section

---
+++ app/static/js/navbar.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const links = document.querySelectorAll('.nav-link');
+
+  // Function to handle the active state
+  function setActiveLink() {
+      const currentLocation = window.location.pathname;
+      links.forEach(link => {
+          link.classList.remove('active');
+          if (link.getAttribute('href') === currentLocation) {
+              link.classList.add('active');
+          }
+      });
+  }
+
+  setActiveLink();
+
+  links.forEach(link => {
+      link.addEventListener('click', function() {
+          links.forEach(l => l.classList.remove('active'));
+          this.classList.add('active');
+      });
+  });
+});
