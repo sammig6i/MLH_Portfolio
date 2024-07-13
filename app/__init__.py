d1b90938b997f47232020c28d8cc2164c38821ff
SavviSam <swghazzawi@gmail.com>
2024-07-12 20:17:53 -0600

    fix db

--- app/__init__.py
+++ app/__init__.py
@@ -13,7 +13,8 @@ mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
 user=os.getenv("MYSQL_USER"),
 password=os.getenv("MYSQL_PASSWORD"),
 host=os.getenv("MYSQL_HOST"),
-port=3306)
+port=5000)
+
 
 class TimeLinePost(Model):
     name = CharField()
