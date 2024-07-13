941b158df27a4e873e3918501891b156825bfe8d
SavviSam <swghazzawi@gmail.com>
2024-07-12 19:33:25 -0600

    aadd endpoints for db

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,10 @@
+from crypt import methods
 import os
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
 from peewee import *
+import datetime
+from playhouse.shortcuts import model_to_dict
 
 load_dotenv()
 app = Flask(__name__)
@@ -12,9 +15,17 @@ password=os.getenv("MYSQL_PASSWORD"),
 host=os.getenv("MYSQL_HOST"),
 port=3306)
 
-print(mydb)
+class TimeLinePost(Model):
+    name = CharField()
+    email = CharField()
+    content = TextField()
+    created_at = DateTimeField(default=datetime.datetime.now)
 
+    class Meta:
+        database = mydb
 
+mydb.connect()
+mydb.create_tables([TimeLinePost])
 
 
 @app.route('/')
@@ -62,3 +73,28 @@ def hobbies():
         Hobby("Hiking", "I love to be outside.", "../static/img/hiking.jpg")
     ]
     return render_template('hobbies.html', title="My Hobbies", url=os.getenv("URL"), hobbies=hobbies)
+
+
+@app.route('/api/timeline_post', methods=['POST'])
+def post_time_line_post():
+    name = request.form['name']
+    email = request.form['email']
+    content = request.form['content']
+    timeline_post = TimeLinePost.create(name=name, email=email, content=content)
+
+    return model_to_dict(timeline_post)
+
+
+@app.route('/api/timeline_post', methods=['GET'])
+def get_time_line_post():
+    return {
+        'timeline posts': [
+            model_to_dict(p)
+            for p in
+            TimeLinePost.select().order_by(TimeLinePost.created_at.desc())
+        ]
+    }
+
+
+
+    
