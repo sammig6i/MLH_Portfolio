11fa2e520304488a583911712235710ba647d36e
SavviSam <swghazzawi@gmail.com>
2024-07-12 19:44:19 -0600

    test script

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -6,9 +6,9 @@ cd /root/MLH_Portfolio
 
 git fetch && git reset origin/main --hard
 
-# source python3-virtualenv/bin/activate
+source python3-virtualenv/bin/activate
 
-# pip install -r requirements.txt
+pip install -r requirements.txt
 
-tmux new-session -d -s flask-server "cd /root/MLH_Portfolio && source /root/MLH_Portfolio/python3-virtualenv/bin/activate && flask run --host=0.0.0.0"
+tmux new-session -d -s flask-server "flask run --host=0.0.0.0"
 
