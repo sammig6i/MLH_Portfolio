41866dd3c297126cc70356c3d66c3c7bcd3024b9
SavviSam <swghazzawi@gmail.com>
2024-07-12 19:38:14 -0600

    aadd endpoints for db

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -8,7 +8,7 @@ git fetch && git reset origin/main --hard
 
 source python3-virtualenv/bin/activate
 
-pip install -r requirements.txt
+# pip install -r requirements.txt
 
 tmux new-session -d -s flask-server "cd /root/MLH_Portfolio && source /root/MLH_Portfolio/python3-virtualenv/bin/activate && flask run --host=0.0.0.0"
 
