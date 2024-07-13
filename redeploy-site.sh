9f27dcaa7f9e9c99727a983f0e203d603dc36edb
SavviSam <swghazzawi@gmail.com>
2024-07-12 19:51:53 -0600

    test script

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -10,5 +10,7 @@ source python3-virtualenv/bin/activate
 
 pip install -r requirements.txt
 
-tmux new-session -d -s flask-server "flask run --host=0.0.0.0"
+tmux new-session -d -s flask-server 
+
+flask run --host=0.0.0.0
 
