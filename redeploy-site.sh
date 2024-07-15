0ae24cc348fa31c41788096b4204ecee69ecd7af
SavviSam <swghazzawi@gmail.com>
2024-07-15 13:46:27 -0600

    update redeploy script for portfolio service

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-pkill -f tmux
-
 cd /root/MLH_Portfolio 
 
 git fetch && git reset origin/main --hard
@@ -10,5 +8,4 @@ source python3-virtualenv/bin/activate
 
 pip install -r requirements.txt
 
-tmux new-session -d -s flask-server "cd /root/MLH_Portfolio && flask run --host=0.0.0.0"
-
+sudo systemctl restart myportfolio
